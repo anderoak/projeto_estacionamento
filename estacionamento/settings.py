2ce1639d128b2c275c5f4558614a350420452650
Anderson de Carvalho <anderson.internetwork@gmail.com>
2019-04-08 13:17:45 -0300

    Implementa layout da página home do website

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "website/static"),
 ]
 
 LOGIN_URL = 'home'
